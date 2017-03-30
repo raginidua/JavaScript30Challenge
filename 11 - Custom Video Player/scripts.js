2bb7a845cf662428b5a04f7cc8fc5a773a214b93
raginidua <raginidua1@gmail.com>
2017-03-30 11:48:20 +0100

    Added the skip button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,6 +8,7 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build our functions
+// This functions is connecting to the hooking up of the event listener below - it allows us to pause and play the video by clicking the video or the play button.
 function togglePlay(){
   if (video.paused) {
     video.play();
@@ -16,14 +17,24 @@ function togglePlay(){
   }
 }
 
+// This function is connected to the play button and whether it shows a pause or play icon depending on whether the video is pause or play. We can use 'this', because 'this' will always be the video itself, which has the property .paused, which we can check for.
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   console.log(icon);
   toggle.textContent = icon;
 }
 
+// This function relates to the skip button which enables us to go forward 25seconds or back 10 seconds. The way to define how much the video will be skipped, go to the DOM elements, and we have a data-skip of 10seconds and data-skip of 25 seconds.  We can therefore put this on anything. So we wil listen for anything which has a data-skip attribute.
+function skip(){
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // Hook up the event listener.
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
